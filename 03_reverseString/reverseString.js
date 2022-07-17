c7331dc1c2c65641ee42d2f0ffabbaf0f3a23cca
DeadByBattery <owkward.games@gmail.com>
2022-07-17 14:04:39 +0800

    Solve '03-task'

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  // let string_to_array = [];
+  // let array_to_string = "";
+  // for (const char of str) {
+  //   string_to_array.push(char);
+  // }
+  // string_to_array.reverse();
+  // for (const item of string_to_array) {
+  //   array_to_string += item;
+  // }
+  // return array_to_string;
+  return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
