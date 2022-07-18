14889ed0bae86c7688b2af5f034092d4c93835c0
DeadByBattery <owkward.games@gmail.com>
2022-07-18 10:16:38 +0800

    Solve '05-task'

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (start < 0 || end < 0) return "ERROR";
+  if (typeof start != "number" || typeof end != "number") return "ERROR";
+  if (start > end) {
+    const temp = end;
+    end = start;
+    start = temp;
+  }
 
+  let result = 0;
+  for (let i = start; i <= end; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
