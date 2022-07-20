177e6000a3abcc45e051e211d6e76ea2cebbeb86
DeadByBattery <owkward.games@gmail.com>
2022-07-20 09:32:25 +0800

    Solve '07-task'

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (f) {
+  const celcius = (f - 32) * 0.5556;
+  return Number(celcius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (c) {
+  const fahrenheit = c * 1.8 + 32;
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
