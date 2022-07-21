774f049ea2678fd1e48938a6fea8ac239b6d86e3
DeadByBattery <owkward.games@gmail.com>
2022-07-21 19:59:44 +0800

    Solve '10-task'

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (nterms) {
+  let n1 = 0;
+  let n2 = 1;
+  let count = 1;
+  nterms = parseInt(nterms);
 
+  if (nterms < 0) return "OOPS";
+  if (nterms === 1) return 1;
+  while (count < nterms) {
+    nth = n1 + n2;
+    n1 = n2;
+    n2 = nth;
+    count++;
+  }
+  return nth;
 };
 
 // Do not edit below this line
