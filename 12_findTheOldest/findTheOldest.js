d3ea5e3931c90d10380355eba8bd14f6944777c7
DeadByBattery <owkward.games@gmail.com>
2022-07-22 11:00:11 +0800

    Solve '12-task'
    Minimum viable solution lezgoooo
    Changes that I want:
    * No using/making any other array
    * Just using the array given

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const today = new Date();
+const year = today.getFullYear();
 
+const findTheOldest = function (people) {
+  let calcd_age = [];
+  for (const person of people) {
+    if (!("yearOfDeath" in person)) {
+      const total_age = year - person.yearOfBirth;
+      calcd_age.push(total_age);
+    } else {
+      const total_age = person.yearOfDeath - person.yearOfBirth;
+      calcd_age.push(total_age);
+    }
+  }
+  const index = calcd_age.indexOf(Math.max(...calcd_age));
+  return people[index];
 };
 
 // Do not edit below this line
